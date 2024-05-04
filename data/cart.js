4fb558c2e8b9a6746fba0f7e2b0986a38a892c41
diegoo <drafael038@insite.4cd.edu>
2024-05-03 19:48:19 -0700

    making delivery option interactive

--- data/cart.js
+++ data/cart.js
@@ -2,12 +2,14 @@ export let cart = JSON.parse(localStorage.getItem('cart'));
 
 if (!cart) {
     cart = [{
-        id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity: 2
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+        deliveryOptionId: 1
     }, 
     {
-        id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity: 1
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1,
+        deliveryOptionId: 2
     }];
 }
 
@@ -83,6 +85,20 @@ export function updateProductQuantity(productId, newQuantity) {
             item.quantity = Number(newQuantity)
         }
     })
+
+    saveToStore()
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+    let matchingItem;
+
+    cart.forEach((item) => {
+        if (productId === item.productId) {
+            matchingItem = item
+        }
+    })  
+
+    matchingItem.deliveryOptionId = Number(deliveryOptionId)
 
     saveToStore()
 }
